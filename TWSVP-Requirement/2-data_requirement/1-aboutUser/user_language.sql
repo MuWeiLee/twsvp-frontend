99952eac5afb6f5c0538b696d9c028df7a6af71d
MuWeiLee <leemuwei0227@gmail.com>
2026-01-30 22:55:07 +0800

    删除部分需求文档

--- TWSVP-Requirement/2-data_requirement/1-aboutUser/user_language.sql
+++
@@ -1,6 +0,0 @@
--- add language preference to users/profiles
-alter table public.users
-add column if not exists language text;
-
-alter table public.profiles
-add column if not exists language text;
